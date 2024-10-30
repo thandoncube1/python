bb94b7a8cf52323ba3fbb37cf80910d1b9bf26ce
thandoncube1 <ncubethando20@gmail.com>
2024-10-30 03:11:32 -0400

    Initial commit - python

---
+++ sampleWeb/main.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/shop")
+def shop():
+    return render_template('shop.html')
+
+if __name__ == "__main__":
+    index()
+    app.run(debug=True)
