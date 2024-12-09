5719bdce4421e6e63a01437e3ee1a9e30824f757
thandoncube1 <ncubethando20@gmail.com>
2024-12-08 23:49:22 -0500

    Adding comments to the bubble sort method.

--- data_structures/bubble_sort.py
+++ data_structures/bubble_sort.py
@@ -3,9 +3,14 @@ from random import shuffle
 
 def bubble_sort(array: list, n: int) -> None:
     for _ in range(0, n):
+        # initial loop just to iterate over all elements
         for i in range(0, n-1):
+            # Loop for comparing until the second of last element in the array
+            # This way is doesn't go out of range.
             if array[i] > array[i+1]:
+                # Compare the current element with the next element in the list
                 array[i], array[i+1] = array[i+1], array[i]
+                # Swap the elements using the pattern matching operation.
 
 
 
