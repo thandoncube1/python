e8136b6503528d14313fcb4a09c79b6fe9a3d86f
thandoncube1 <ncubethando20@gmail.com>
2024-12-24 00:09:16 -0500

    Working on the selection sort - sorting in general.

--- pythonTests/searching.py
+++ pythonTests/searching.py
@@ -9,8 +9,19 @@ def bubble_sort(L):
                 L[j], L[j - 1] = L[j - 1], L[j]
 
 
+# Selection sort
+""" select the smallest in the list and put it at thhe front of the list """
+def selection_sort(L):
+    suffixStart = 0
+    while suffixStart != len(L):
+        for i in range(suffixStart, len(L)):
+            if L[i] < L[suffixStart]:
+                L[i], L[suffixStart] = L[suffixStart], L[i]
+        suffixStart += 1
+
 
 
 arrayList = [3,6,8,9,3,1,4,5,12,7,2]
-bubble_sort(arrayList)
+# bubble_sort(arrayList)
+selection_sort(arrayList)
 print(arrayList)
