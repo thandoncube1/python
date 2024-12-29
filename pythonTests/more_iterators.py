3f855d1ef1159d72e5d1bd743092793cef459a11
thandoncube1 <ncubethando20@gmail.com>
2024-12-29 17:16:09 -0500

    Working with itertools for more advanced printing.

--- pythonTests/more_iterators.py
+++ pythonTests/more_iterators.py
@@ -1,5 +1,6 @@
 # Example file for Advanced Python by Joe Marini
 
+import itertools
 
 # define a list of days in English and French
 days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
@@ -14,12 +15,19 @@ for i, d in enumerate(days, start=1):
     print(i, d)
 
 # use zip to combine sequences
+for d in zip(days, daysFr):
+    print(d)
 
-
+print("="*20)
 # use enumerate and zip together
-
+for i, d in enumerate(zip(days, daysFr), start=1):
+    print(i, d[0], "=", d[1], "in French")
 
 # use zip_longest
 seq1 = ["A","B","C","D","E","F"]
 seq2 = [1, 2, 3, 4]
 seq3 = "xyz"
+
+result = itertools.zip_longest(seq1, seq2, seq3, fillvalue="-")
+print("Result: ")
+[print(item, end="\n") for item in result]
