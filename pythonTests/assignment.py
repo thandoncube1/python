36c18ac5f86d8bd6874bb228f3494488c331e577
thandoncube1 <ncubethando20@gmail.com>
2024-12-29 16:46:00 -0500

    Working more with the walrus operator for more advanced operations

--- pythonTests/assignment.py
+++ pythonTests/assignment.py
@@ -15,16 +15,16 @@ print(x)
 
 # How does thhis help when you are writing code
 # The assignment expression is useful for writing concise code
-while (thestr := input("Value? ")) != "exit":
-    """Assign and make use of the value in the condition"""
-    print(thestr)
+# while (thestr := input("Value? ")) != "exit":
+    # """Assign and make use of the value in the condition"""
+    # print(thestr)
 
 # The walrus operator can help reduce redundant function calls
 values = [12, 0, 10, 5, 9, 18, 41, 23, 30, 16, 18, 9, 18, 22]
-l = len(values)
-s = sum(values)
 val_data = {
-    "length": l,
-    "total": s,
+    "length": (l := len(values)),
+    "total": (s := sum(values)),
     "average": s/l
 }
+
+pprint.pp(val_data)
