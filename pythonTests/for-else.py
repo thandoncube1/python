75e2d7d9f08b66bc5881570a853f3ca6222aa7f2
thandoncube1 <ncubethando20@gmail.com>
2024-12-29 17:20:39 -0500

    Working with the else on the loop.

---
+++ pythonTests/for-else.py
@@ -0,0 +1,30 @@
+# Example file for Advanced Python by Joe Marini
+# The for-else loop construct
+
+names = ["Jim", "Pam", "Creed", "Michael", "Dwight", "Oscar", "Kevin", "Phyllis"]
+
+# The else clause on a for loop is only executed if the loop completes every iteration
+def findname(target):
+    for name in names:
+        if name == target:
+            print("Name found");
+            return True
+    else:
+        print("Name not found")
+        return False
+
+print(findname("Creed"))
+print(findname("Tom"))
+
+# Check if a number is prime
+def isPrime(num):
+    for i in range(2, num):
+        if num % i == 0:
+            print(num, "is not a prime number")
+            break
+    else:
+        print(num, "is a prime number")
+
+
+isPrime(31)
+isPrime(56)
