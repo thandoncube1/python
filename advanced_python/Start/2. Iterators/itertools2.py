a0b7b63b984e42f04fa65e76180a13f84ac03108
thandoncube1 <ncubethando20@gmail.com>
2025-01-13 14:49:04 -0500

    Working with from_iterable - itertools

--- advanced_python/Start/2. Iterators/itertools2.py
+++ advanced_python/Start/2. Iterators/itertools2.py
@@ -6,16 +6,19 @@ import itertools
 
 # chain() creates a single iterable from multiple
 x = itertools.chain("ABCD", "1234")
-print(list(x))
+# print(list(x))
 
 # make a prepend function
 def prepend(val, iterable):
     return itertools.chain([val], iterable)
 
 
-print(list(prepend("a", "bcde")))
+# print(list(prepend("a", "bcde")))
 
 # chain.from_iterable is an alternate usage of chain
 s1 = "ABCDEFG"
 s2 = [1,2,3,4,5]
 s3 = ['$','%','@','&']
+
+result = itertools.chain.from_iterable([s1, s2, s3])
+print(list(result))
