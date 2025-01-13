3b57aa81a47e4ee21cbadc2e9d49dfe90f4255e4
thandoncube1 <ncubethando20@gmail.com>
2025-01-13 14:47:35 -0500

    Working with chain - itertools

--- advanced_python/Start/2. Iterators/itertools2.py
+++ advanced_python/Start/2. Iterators/itertools2.py
@@ -5,10 +5,15 @@ import itertools
 
 
 # chain() creates a single iterable from multiple
-
+x = itertools.chain("ABCD", "1234")
+print(list(x))
 
 # make a prepend function
+def prepend(val, iterable):
+    return itertools.chain([val], iterable)
 
+
+print(list(prepend("a", "bcde")))
 
 # chain.from_iterable is an alternate usage of chain
 s1 = "ABCDEFG"
